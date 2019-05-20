e33b9c3489479d33f0a995b208862f83c5a88980
Fabio Francisco campelo <fabiocampello@outlook.com.br>
2019-05-20 19:21:34 -0300

    Criando projeto

---
+++ src/main/java/com/virtual/loja/anything/LojaVirtualAnythingApplication.java
@@ -0,0 +1,13 @@
+package com.virtual.loja.anything;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualAnythingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualAnythingApplication.class, args);
+	}
+
+}
