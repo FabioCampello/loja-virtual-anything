7ff32f21d4f41226a37ee0dafcbd2420c0aa10bd
Fabio Francisco campelo <fabiocampello@outlook.com.br>
2019-05-25 23:54:51 -0300

    Camada Controller

---
+++ src/main/java/com/virtual/loja/anything/controller/EstoqueController.java
@@ -0,0 +1,19 @@
+package com.virtual.loja.anything.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Descrição: End-point da api responsável por requisicoes de Estoque.
+ * 
+ * @author "Fábio Campêllo"
+ * @since 25/05/2019
+ * @version 0.0.1
+ */
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("api/estoque")
+public class EstoqueController {
+
+}
